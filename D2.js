6f7a162a4b839a382870c3e18e591082ef771281
RiccardoAD <rick_95@libero.it>
2024-01-23 17:01:41 +0100

    second.commit

--- D2.js
+++ D2.js
@@ -21,12 +21,11 @@ if(num1>num2){
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 const num3=7;
-const num4=29;
 
 if(num3!==5){
-  console.log(" il numero fornito è diverso da 5"+"","not equal")
+  console.log(" not equal")
 } else {
-  console.log()
+  console.log( "equal")
 }
 
 /* ESERCIZIO 3
@@ -35,11 +34,29 @@ if(num3!==5){
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+ const num4= 25;
+ if (num4% 5===0){
+  console.log(" divisibile per 5");
+ } else {
+  console.log(" non divisibile per 5");
+ }
 /* ESERCIZIO 4
   Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+const num5=4;
+const num6=4;
+if(num5 + num6 ===8) {
+  console.log("la loro addizione è uguale a 8");
+} else if(num5 - mum6===8) {
+ console.log("la loro sottrazione  è uguale a 8");
+} else{
+  console.log("NOn è uguale a 8");
+}
+
+
 
 /* ESERCIZIO 5
   Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
@@ -49,12 +66,23 @@ if(num3!==5){
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+const prodotto=100;
+const totalShoppingCart=prodotto;
+const costShip= 10;
+if(totalShoppingCart >50){
+  console.log("spedizione gratuita");
+} else if(totalShoppingCart<50) {
+  console.log( "non hai superato la soglia di 50 euro per la spedione gratuita, la tua spedione avrà un costo fisso pari a ",
+  );
+}
+
 /* ESERCIZIO 6
   Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
   Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando come prima se le spedizioni sono gratuite oppure no e e calcolando il totale.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
 
 /* ESERCIZIO 7
   Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
@@ -67,7 +95,10 @@ if(num3!==5){
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
 */
-
+const num10=25;
+const num11="25";
+console.log( typeof num10);
+console.log( typeof num11);
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 9
@@ -75,6 +106,14 @@ if(num3!==5){
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+const num12=22;
+
+if(num12 %2 ===0){
+  console.log("è pari",num12 );
+} else {
+  console.log( "è dispari",num12);
+}
+
 
 /* ESERCIZIO 10
   Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
@@ -89,6 +128,14 @@ if(num3!==5){
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let val=7;
+if(val<5){
+  console.log( "meno di 5");  
+}else if (val<10) {
+  console.log( "meno di 10");
+}else {
+  console.log("uguale a 10 o maggiore");
+}
 
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
